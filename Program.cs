8ba8711641b7cde95c7b69975a368e21e3b4f8ff
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-17 15:02:27 +0100

    exercice 04 non corrigé

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string genre = "Monsieur";
+            string nom = "Vincent";
+            int visit = 1;
+
+            Console.WriteLine($"Bonjour {genre} {nom}, vous êtes venu nous rendre visite {visit} fois !");
+            Console.ReadKey();
+        }
+    }
+}
